f64862ecc09adbdee2c9e2eac10bc6765ab6d44e
maksimblankov <maxblankov436@gmail.com>
2023-02-04 11:45:26 +0200

    Init config-server module

---
+++ config-server/src/test/java/ua/cheesecake/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.cheesecake.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
