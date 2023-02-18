b9073272380db535ac5abf731795ec29ecebcdbe
Misha <miha.18012006@gmail.com>
2023-02-18 18:14:58 +0100

    set up deployments

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+J
+config-server/build.gradle,9\e\9e9343d55cfc75598e543414688c2fc07bb5b139
+L
+common-protocol/build.gradle,5\3\53cf8e9dd5fe83d674ed6591a83bd8299b6b6249
+J
+eureka-server/build.gradle,9\2\926f3c0072d8ffed034ab5418b00a4ab803f7479
---
+++ user-service/src/test/java/com/user/sevice/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.sevice.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
