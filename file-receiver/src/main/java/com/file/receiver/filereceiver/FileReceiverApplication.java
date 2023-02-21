47cb009cf0834850d3faba4c62fdec3bf107f171
Misha <miha.18012006@gmail.com>
2023-02-21 22:42:43 +0100

    Create file-receiver module

---
+++ file-receiver/src/main/java/com/file/receiver/filereceiver/FileReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.file.receiver.filereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileReceiverApplication.class, args);
+    }
+
+}
