5b32ec7e6eda2f715d890b36eb810a6c3540ecc8
maksimblankov <maxblankov436@gmail.com>
2023-02-23 00:36:06 +0200

    delete Jacksonized annotation

--- common-protocol/src/main/java/dto/PhotoDto.java
+++ common-protocol/src/main/java/dto/PhotoDto.java
@@ -2,11 +2,9 @@ package dto;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
-@Jacksonized
 public class PhotoDto {
     private String photoName;
     private Byte file;
