f8ab18949cbcafc5df288a16e2935e8abb324894
Misha <miha.18012006@gmail.com>
2023-02-25 23:39:14 +0100

    add Jacksonized annotation #ThanksMaxim

--- common-protocol/src/main/java/dto/PhotoDto.java
+++ common-protocol/src/main/java/dto/PhotoDto.java
@@ -2,9 +2,11 @@ package dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
 public class PhotoDto {
     private String photoName;
     private Byte file;
