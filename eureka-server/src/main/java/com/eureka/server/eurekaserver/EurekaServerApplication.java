a89f846bfb9910e40a8642d1cab083b6bcf7de29
Misha <miha.18012006@gmail.com>
2023-03-01 17:06:40 +0100

    Fix eureka-server gradle

--- eureka-server/src/main/java/com/eureka/server/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/eureka/server/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.eureka.server.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/eureka/server/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/eureka/server/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.eureka.server.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
