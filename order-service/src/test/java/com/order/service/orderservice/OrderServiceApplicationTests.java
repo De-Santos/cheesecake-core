22eec6de50e6bd196992d0af8aee54bdb9f88f96
Misha <miha.18012006@gmail.com>
2023-03-02 12:15:07 +0100

    Create empty order-service

---
+++ order-service/src/test/java/com/order/service/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.service.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
