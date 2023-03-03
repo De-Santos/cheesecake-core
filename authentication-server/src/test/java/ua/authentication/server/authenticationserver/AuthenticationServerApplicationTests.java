f72c3b09161f07134fadee9b5d251fd72a6ee894
Misha <miha.18012006@gmail.com>
2023-03-03 13:45:54 +0100

    Create authentication-server

---
+++ authentication-server/src/test/java/ua/authentication/server/authenticationserver/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.authentication.server.authenticationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
