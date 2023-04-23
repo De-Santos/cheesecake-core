3ebaa3c08947520351d9285628b906d99f6d8da3
Misha <miha.18012006@gmail.com>
2023-04-23 15:19:15 +0200

    Update `AutoBuild`

--- product-service/docker/AutoBuild.sh
+++ product-service/docker/AutoBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Running my Python script..."
+echo "AUTO BUILD START..."
 python3 /Users/your_username/CodeFile/JavaProjects/cheesecake-core/product-service/docker/AutoBuilder/builder.py
-echo "Finished running my Python script."
+echo "AUTO BUILD COMPLETED!"
