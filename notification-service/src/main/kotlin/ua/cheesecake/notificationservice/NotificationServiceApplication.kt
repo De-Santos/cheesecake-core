910c1f61e18c21ed119a89284088040492ddf643
maksimblankov <maxblankov436@gmail.com>
2023-04-30 13:38:16 +0300

    init notification service

---
+++ notification-service/src/main/kotlin/ua/cheesecake/notificationservice/NotificationServiceApplication.kt
@@ -0,0 +1,11 @@
+package ua.cheesecake.notificationservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationServiceApplication>(*args)
+}
