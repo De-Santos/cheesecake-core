ea18eddf7afffa7d878c7193be1d0f613f9cc82a
Misha <miha.18012006@gmail.com>
2023-05-07 15:14:55 +0200

    remove useless projects

--- file-receiver/src/main/java/com/file/receiver/exceptions/dto/ExceptionDto.java
+++
@@ -1,8 +0,0 @@
-package com.file.receiver.exceptions.dto;
-
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
-
-public record ExceptionDto(String exceptionName, String message, HttpStatus httpStatus, LocalDateTime time) {
-}
